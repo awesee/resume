6df7fb0c70fa0f1440fbb0d751b760bc718cca2b
Openset <openset@users.noreply.github.com>
2017-09-26 09:59:51 +0800

    js

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,9 +1,9 @@
-if (window.location.href.indexOf("mobile") == -1) {
-    if (document.body.clientWidth < 702) {
-        window.location.href = "/mobile/";
-    }
-} else {
-    if (document.body.clientWidth > 702) {
-        window.location.href = "/";
-    }
-}
+//if (window.location.href.indexOf("mobile") == -1) {
+//    if (document.body.clientWidth < 702) {
+//        window.location.href = "/mobile/";
+//    }
+//} else {
+//    if (document.body.clientWidth > 702) {
+//        window.location.href = "/";
+//    }
+//}
